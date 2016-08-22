c16658b5c24d3ec04c410397b2f0bd8d6bc62f01
YorkZero <yorkzero8312@gmail.com>
2016-08-22 19:26:35 +0900

    14
    14

---
+++ VRProject/Assets/WindSoundScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WindSoundScript : MonoBehaviour {
+
+    public GameObject car;
+    private AudioSource windSound;
+    private Vector3 lastPosition;
+    private float fixPicth = 0.4f;
+    private float fixVolume = 0.3f;
+	// Use this for initialization
+	void Start () {
+        windSound = gameObject.GetComponent<AudioSource>();
+        lastPosition = car.transform.position;
+        //Debug.Log(lastPosition);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 nowPosition = car.transform.position;
+        float distancePerFrame = Vector3.Distance(nowPosition, lastPosition);
+        //Debug.Log(distancePerFrame);
+        float fixPictchFinal = fixPicth + distancePerFrame * 1;
+        float fixVolumeFinal = fixVolume + distancePerFrame * 3;
+
+
+        //Debug.Log(fixPictchFinal);
+        windSound.pitch = fixPictchFinal;
+        windSound.volume = fixVolumeFinal;
+
+        lastPosition = nowPosition;
+
+	}
+}
---
+++ VRProject/Assets/trainSoundScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class trainSoundScript : MonoBehaviour {
+
+    public GameObject car;
+    private AudioSource trainSound;
+    private Vector3 lastPosition;
+    private float fixPicth = 0.4f;
+    private float fixVolume = 0.5f;
+    // Use this for initialization
+    void Start () {
+        trainSound = gameObject.GetComponent<AudioSource>();
+        lastPosition = car.transform.position;
+        //Debug.Log(lastPosition);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 nowPosition = car.transform.position;
+        float distancePerFrame = Vector3.Distance(nowPosition, lastPosition);
+        Debug.Log(distancePerFrame);
+        float fixPictchFinal = fixPicth + distancePerFrame * 2;
+        
+
+        float fixVolumeFinal = distancePerFrame * 40;
+
+        //Debug.Log(fixPictchFinal);
+        trainSound.pitch = fixPictchFinal;
+        trainSound.volume = fixVolumeFinal;
+
+        lastPosition = nowPosition;
+    }
+}
